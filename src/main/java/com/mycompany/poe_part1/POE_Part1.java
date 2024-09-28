ae5ef1d302df6c5f29c003a4e5f1f57980b7f83c
RC_Student_lab <RC_Student_lab@10.0.0.99>
2024-09-28 10:13:10 +0000

    username

--- src/main/java/com/mycompany/poe_part1/POE_Part1.java
+++ src/main/java/com/mycompany/poe_part1/POE_Part1.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.poe_part1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author RC_Student_lab
@@ -11,6 +13,23 @@ package com.mycompany.poe_part1;
 public class POE_Part1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+         //Declarations 
+        String username;
+        
+        //Object of the scanner
+        Scanner sc = new Scanner(System.in);
+        
+        //Prompt
+        System.out.print("Enter your username: ");
+        username = sc.next();
+        
+        //if statement
+        if (username.contains("_") && username.length() <= 5) {
+            System.out.println("username successfully captured");
+        } else {
+            System.out.println("username is not correctly formatted, please ensure that your username contains an underscore and is no more than 5 characters in length");
+        
+        }
     }
-}
+    }
+
