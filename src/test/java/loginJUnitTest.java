41ec1cfd36b7bf944ea062b091d0761a30fa06a6
RC_Student_lab <RC_Student_lab@10.0.0.99>
2024-09-29 08:29:17 +0000

    JUnit tests

--- src/test/java/loginJUnitTest.java
+++ src/test/java/loginJUnitTest.java
@@ -45,37 +45,37 @@ public class UserAccountTest {
     @Test
     public void testRegisterUser_Successful() {
         String result = userAccount.registerUser("kyl_1", "Ch&&sec@ke99!", "Kyle", " Kunutu");
-        assertEquals("Login successful.", result, "True");
+        assertTrue(result.equals("Login successful."), "True");
     }
 
     @Test
     public void testRegisterUser_InvalidUsername() {
         String result = userAccount.registerUser("kyle!!!!!!!", "Password1", "Kyle", " Kunutu");
-        assertEquals("Login failed", result, "False");
+        assertTrue(result.equals("Login failed"), "False");
     }
 
     @Test
     public void testRegisterUser_InvalidPassword() {
         String result = userAccount.registerUser("kyl_1", "Ch&&sec@ke99!", "Kyle", " Kunutu");
-        assertEquals("Username correctly formatted.", result, "True");
+        assertTrue(result.equals("Username correctly formatted."), "True");
     }
 
     @Test
     public void testLoginUser_Successful() {
         userAccount.registerUser("Kyle!!!!!!!", "Password1", "Kyle", " Kunutu");
-        assertEquals(true, userAccount.loginUser("Kyle!!!!!!!", "Ch&&sec@ke99!"), "Username incorrectly formatted.");
+        assertTrue(userAccount.loginUser("Kyle!!!!!!!", "Ch&&sec@ke99!"), "Username incorrectly formatted.");
     }
 
     @Test
     public void testLoginUser_InvalidUsername() {
         userAccount.registerUser("Lerato", "Password", "Lerato", "Plank");
-        assertEquals(false, userAccount.loginUser("Lerato", "Password44"), "Incorrect password.");
+        assertTrue(!userAccount.loginUser("Lerato", "Password44"), "Incorrect password.");
     }
 
     @Test
     public void testLoginUser_InvalidPassword() {
         userAccount.registerUser("Lea_1", "Password$$!", "Lea", "Plank");
-        assertEquals(false, userAccount.loginUser("Lea_1", "Password$$!"), "Incorrect password.");
+        assertTrue(!userAccount.loginUser("Lea_1", "Password$$!"), "Incorrect password.");
     }
 
 }
