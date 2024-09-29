ae128215781a285d843bc11564cb877211097e38
RC_Student_lab <RC_Student_lab@10.0.0.99>
2024-09-29 00:06:05 +0000

    Login/registerUser

--- src/main/java/com/mycompany/poe_part1/POE_Part1.java
+++ src/main/java/com/mycompany/poe_part1/POE_Part1.java
@@ -22,9 +22,10 @@ public class POE_Part1 {
         //Create an object for your Scanner
         Scanner sc = new Scanner(System.in);
         
-        //Object of the UserValidator and PasswordValidator classes
+        //Object of the UserValidator, PasswordValidator and UserAccount classes
         UsernameValidator usernameValidator = new UsernameValidator();
         PasswordValidator passwordValidator = new PasswordValidator();
+        UserAccount userAccount = new UserAccount();
         
          // Prompt for username
         System.out.print("Enter your username: ");
@@ -33,6 +34,13 @@ public class POE_Part1 {
         System.out.print("Enter your password: ");
         password = sc.next();
         
+        // Prompt the user for first name and last name
+        System.out.print("Enter your first name: ");
+        firstName = sc.next();
+        
+        System.out.print("Enter your last name: ");
+        lastName = sc.next();
+
           // Validate username
         if (usernameValidator.checkUserName(username)) {
             System.out.println("Username successfully captured.");
@@ -47,17 +55,27 @@ public class POE_Part1 {
         } else {
             System.out.println("Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a number, and a special character.");
             return; // Exit if password is invalid
-        }
-        
-        // Prompt for first name and last name
-        System.out.print("Enter your first name: ");
-        firstName = sc.next();
-        
-        System.out.print("Enter your last name: ");
-        lastName = sc.next();
-        
-         // successful message after user successfully creates an account
+        }    
+        // successful message after user successfully creates an account
         System.out.println("Account created successfully for " + firstName + " " + lastName + ".");
+        
+        // enter the users information so that the user is able to login
+        String registrationMessage = userAccount.registerUser(username, password, firstName, lastName);
+        System.out.println(registrationMessage);
+
+        // Prompt the user to login
+        System.out.print("Enter your username to login: ");
+        String loginUsername = sc.next();
+
+        System.out.print("Enter your password to login: ");
+        String loginPassword = sc.next();
+
+        // Verify the information entered
+        if (userAccount.login(loginUsername, loginPassword)) {
+            System.out.println("Welcome " + userAccount.getFullName() + ", it is great to see you again.");
+        } else {
+            System.out.println("Username or password incorrect, please try again.");
+        }
     }
 }
 
